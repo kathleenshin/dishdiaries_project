161307731d674b914101eed11951bf195c64f2f3
kathleen <kathleen.n.shin@gmail.com>
2023-08-26 09:43:43 -0700

    implement getPosts route

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,10 @@
 import express from 'express';
 
-import { getPosts } from '../controllers/posts.js';
+import { getPosts, createPost } from '../controllers/posts.js';
 
 const router = express.Router();
 
-router.get('/', getPosts) => {
-    res.send('THIS WORKS!');
-};
+router.get('/', getPosts);
+router.get('/', createPost);
 
 export default router;
